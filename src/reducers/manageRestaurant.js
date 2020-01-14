1ae73337ec92c6481dac99b976b82da45718a6bd
Michael Schiumo <mschiumo18@gmail.com>
2020-01-14 17:30:02 -0500

    Done.

--- src/reducers/manageRestaurant.js
+++ src/reducers/manageRestaurant.js
@@ -1,50 +1,37 @@
-
 import cuid from 'cuid';
 export const cuidFn = cuid;
 
 export default function manageRestaurants(state = {
   restaurants: [],
-  reviews: []
+  reviews: [],
 }, action) {
-
   switch (action.type) {
 
     case 'ADD_RESTAURANT':
 
-
-    const restaurant = {
-      text: action.text,
-      id: cuid()
-    }
-
-    return {restaurants: state.restaurants.concat(restaurant)}
+      const restaurant = { text: action.text, id: cuidFn() };
+      return {
+        ...state,
+        restaurants: [ ...state.restaurants, restaurant]
+      }
 
     case 'DELETE_RESTAURANT':
       const restaurants = state.restaurants.filter(restaurant => restaurant.id !== action.id);
       return { ...state, restaurants}
 
-
-    const review = {
-      text: action.text,
-      id: cuid(),
-      restaurant_id: restaurant.id
-    }
-
     case 'ADD_REVIEW':
-    debugger
-    console.log("add review reducer", action)
 
-    return {reviews: state.reviews.concat(review)}
+      const review = { text: action.review.text, restaurantId: action.review.restaurantId, id: cuidFn() };
+      return { ...state,
+        reviews: [...state.reviews, review]
+      }
 
     case 'DELETE_REVIEW':
-    console.log("delete review reducer", action)
-
-    return {reviews: state.restaurants.reviews.filter(review => review.id !== action.id && review.restaurant_id !== action.id)}
+      const reviews = state.reviews.filter(review => review.id !== action.id);
+      return {...state, reviews }
 
     default:
-      return state
+      return state;
 
   }
-}
-
-//add actions for reviews
+};
